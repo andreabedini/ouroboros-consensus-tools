22aace40db834f86816e875eeab283bce7c2e9d8
Andrea Bedini <andrea@andreabedini.com>
2024-09-26 14:05:22 +0800

    WIP

--- nixos/default.nix
+++ nixos/default.nix
@@ -8,6 +8,7 @@ in {
   nix.settings = {
     accept-flake-config = true;
     allow-import-from-derivation = true;
+    extra-experimental-features = [ "nix-command flakes" ];
     extra-substituters = "https://cache.iog.io";
     extra-trusted-public-keys = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
   };
